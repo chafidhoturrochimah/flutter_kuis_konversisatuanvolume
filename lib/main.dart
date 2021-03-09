0e3fa73b64621517c3d48547d2e42768ad229ff5
chafidhoturrochimah <chafidhotur96@gmail.com>
2021-03-09 11:15:24 +0700

    Tab Bar

--- lib/main.dart
+++ lib/main.dart
@@ -4,110 +4,95 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class _MyAppState extends State<MyApp> {
+  //controller
+  TextEditingController etInput = new TextEditingController();
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  //variabel berubah
+  double _inputUser = 0;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  //mengeset nilai pada dropdown
+  String _newValue = "Kelvin";
+  double _result = 0;
 
-  final String title;
+  //buat list
+  var listItem = {"Kelvin", "Reamur"};
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  //variable bertipe List<String> (prak 2)
+  // ignore: deprecated_member_use
+  List<String> listViewItem = List<String>();
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  //Fungsi perhitungan suhu perlu untuk diubah sehingga hanya memproses konversi sesuai
+  //dengan pilihan pengguna.
+  void konversiSuhu() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _inputUser = double.parse(etInput.text);
+      if (_newValue == "Kelvin")
+        _result = _inputUser + 273;
+      else
+        _result = (4 / 5) * _inputUser;
+      listViewItem.add("$_newValue : $_result");
     });
   }
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    TabBar myTabBar = TabBar(
+      indicator: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Color(0xff0096ff), Color(0xff6610f2)],
+          begin: FractionalOffset.topCenter,
+          end: FractionalOffset.bottomCenter
+        ),
+        border: Border(bottom: BorderSide(color: Colors.blue[300], width: 5))
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      tabs: <Widget>[
+        Tab(icon: Icon(Icons.repeat, color: Colors.white), text: "Konversi",),
+        Tab(icon: Icon(Icons.history, color: Colors.white), text: "History",)
+      ],
+    );
+
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2, 
+        child: Scaffold(
+        appBar: AppBar(
+          title: Text("Konversi Satuan Volume", style: TextStyle(color: Colors.white),),
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xff0096ff), Color(0xff6610f2)],
+                begin: FractionalOffset.bottomLeft,
+                end: FractionalOffset.topRight
+              )
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          ),
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(myTabBar.preferredSize.height),
+            child: Container(
+              color: Colors.blue[300], 
+              child: myTabBar
+            )
+          ), 
+        ),
+        
+        body: TabBarView(
+          children: <Widget> [
+            //konversi
+            Center(child: Text("Konversi"),),
+
+            //history
+            Center(child: Text("History"),),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
